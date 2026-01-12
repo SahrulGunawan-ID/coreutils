243ab7ef902ba721e6b7fbc08de4f789cb07a072
Sahrul Gunawan ID <sahrulgunawanidgithub@gmail.com>
2026-01-12 07:48:37 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,4 +2,9 @@
 # Github : https://github.com/SahrulGunawan-ID/coreutils
 # GNU Sources Code : https://ftp.gnu.org/gnu/coreutils/coreutils-9.9.tar.xz
 # Thanks For All
+
+# All In bin
 make -j$(nproc) LDFLAGS="-Wl,--strip-all -Wl,--build-id=none -static"
+
+# No All In coreutils only multi call
+#./configure -C --enable-single-binary=symlinks LDFLAGS="-Wl,--strip-all -Wl,--build-id=none -static"
